90b0993cd45d99bcabaa28267a58c90aaf5f6256
Mustafa <mustafagamaldeveloper@gmail.com>
2021-01-20 08:45:40 +0200

    Modify EventListView; modify paginate_by

--- app/events/views.py
+++ app/events/views.py
@@ -22,7 +22,7 @@ class EventListView(ListView):
     template_name = "events/home.html"
     ordering = "date"
     context_object_name = "events"
-    paginate_by = 2
+    paginate_by = 5
 
     def get_queryset(self):
         """
